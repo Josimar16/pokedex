785e584dc5af8271acebcc7454f879b74463f67d
Josimar16 <josimarjr479@gmail.com>
2021-09-10 20:40:47 -0300

    :feat: Criando entity de usuario

--- backend/src/modules/accounts/infra/typeorm/entities/User.ts
+++ backend/src/modules/accounts/infra/typeorm/entities/User.ts
@@ -1,10 +1,28 @@
+import {Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn} from 'typeorm';
+
+@Entity({ schema: 'auth' })
 export class User {
+    @PrimaryColumn('uuid')
     id: string;
+
+    @Column()
     name: string;
-    nickname: string;
+
+    @Column({unique: true, nullable: true})
+    nickname?: string;
+
+    @Column({unique: true})
     email: string;
+
+    @Column({select: false})
     password: string;
-    redefinition_password: boolean;
+
+    @Column({select: false, default: false, nullable: true})
+    redefinition_password?: boolean;
+
+    @CreateDateColumn()
     created_at: Date;
-    updated_at: Date;
+
+    @UpdateDateColumn({nullable: true, select: false})
+    updated_at?: Date;
 }
