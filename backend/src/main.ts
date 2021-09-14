040f48c5fa957db990083a3e494fe3e6bcc94ce7
Josimar Junior <josimarjr479@gmail.com>
2021-09-14 19:10:30 -0300

    :recycle: Adicionando o base URl e habilitando o cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix(process.env.BASE_URL || '/pokedex/api');
+  app.enableCors();
   await app.listen(process.env.APP_PORT, () => {
     console.log(`Application running in port: `, process.env.APP_PORT)
   });
